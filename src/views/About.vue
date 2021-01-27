accf4da52742c98b37c9e2c9eba8dd34c5f33420
Shuanglong <xjshuanglong@126.com>
2021-01-28 01:02:16 +0800

    修改关于页面

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
+<!--
+ * @Author: xautshuanglong
+ * @Date: 2021-01-28 00:58:16
+ * @LastEditor: xautshuanglong
+ * @LastEditTime: 2021-01-28 01:01:59
+ * @FilePath: \shuanglong_admin\src\views\About.vue
+ * @Description: 关于页面
+-->
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page —— Shuanglong</h1>
   </div>
 </template>
