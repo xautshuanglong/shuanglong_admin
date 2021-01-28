31fd8a3b8c687aedeb1ae7bf3f59d4f7007f3605
Shuanglong <xjshuanglong@126.com>
2021-01-28 14:27:50 +0800

    vue3.0 + element-plus

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Author: xautshuanglong
  * @Date: 2021-01-28 00:58:16
  * @LastEditor: xautshuanglong
- * @LastEditTime: 2021-01-28 14:12:19
+ * @LastEditTime: 2021-01-28 14:25:48
  * @FilePath: \shuanglong_admin\src\main.js
  * @Description: main javascript
  */
@@ -12,11 +12,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import 'element-ui/lib/theme-chalk/index.css'
-import ElementUI from 'element-ui'
+import ElementPlus from 'element-plus'
+import lang from 'element-plus/lib/locale/lang/zh-cn'
+import locale from 'element-plus/lib/locale'
+
+locale.use(lang)
 
 createApp(App)
     .use(store)
     .use(router)
-    .use(ElementUI)
+    .use(ElementPlus)
     .mount('#app')
