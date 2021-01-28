cef70f315c15254cdeb51aabad45265090d49585
Shuanglong <xjshuanglong@126.com>
2021-01-28 14:14:07 +0800

    暂时移除 element-ui

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Author: xautshuanglong
  * @Date: 2021-01-28 00:58:16
  * @LastEditor: xautshuanglong
- * @LastEditTime: 2021-01-28 12:39:22
+ * @LastEditTime: 2021-01-28 14:12:19
  * @FilePath: \shuanglong_admin\src\main.js
  * @Description: main javascript
  */
@@ -14,7 +14,6 @@ import store from './store'
 
 import 'element-ui/lib/theme-chalk/index.css'
 import ElementUI from 'element-ui'
-import installElementPlus from './plugins/element'
 
 createApp(App)
     .use(store)
