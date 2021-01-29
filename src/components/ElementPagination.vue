9c2e4091a8721a04aeda495d56df305960d158e6
Shuanglong <xjshuanglong@126.com>
2021-01-29 18:36:34 +0800

    补充分页测试

--- src/components/ElementPagination.vue
+++ src/components/ElementPagination.vue
@@ -2,21 +2,24 @@
  * @Author: xautshuanglong
  * @Date: 2021-01-29 00:51:40
  * @LastEditor: xautshuanglong
- * @LastEditTime: 2021-01-29 17:24:29
+ * @LastEditTime: 2021-01-29 18:35:47
  * @FilePath: \shuanglong_admin\src\components\ElementPagination.vue
  * @Description: ElementPlus data pagination
 -->
 <template>
   <div class="element-pagination">
     <h1>{{ title }}</h1>
-    <el-table :data="tableData">
-        <el-table-column prop="date" label="日期" width="140">
-        </el-table-column>
-        <el-table-column prop="name" label="姓名" width="120">
-        </el-table-column>
-        <el-table-column prop="address" label="地址">
-        </el-table-column>
-    </el-table>
+    <div>
+        <p>每页大小： {{ this.pageSize }}</p>
+        <p>当前页码： {{ this.pageNumber }}</p>
+    </div>
+    <el-pagination layout="prev, pager, next" :total="50" small></el-pagination>
+    <el-pagination layout="prev, pager, next" :total="50"></el-pagination>
+    <el-pagination layout="prev, pager, next" :total="500"></el-pagination>
+    <el-pagination layout="prev, pager, next" :total="500" background></el-pagination>
+    <el-pagination layout="total, prev, pager, next, sizes, jumper" :total="500" background></el-pagination>
+    <el-switch v-model="hideSinglePage" active-text="单页隐藏" inactive-text="单页显示"></el-switch>
+    <el-pagination layout="prev, pager, next" :total="10" :hide-on-single-page="hideSinglePage"></el-pagination>
   </div>
 </template>
 
@@ -27,13 +30,10 @@ export default {
         title: String
     },
     data () {
-        const item = {
-            date: '2021-01-01',
-            name: '双龙',
-            address: '广东省深圳市南山区'
-        }
         return {
-            tableData: Array(20).fill(item)
+            pageSize: 0,
+            pageNumber: 0,
+            hideSinglePage: false
         }
     }
 }
