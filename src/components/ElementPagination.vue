5d581ecadc484b517ba8780af71d52b9079b7eab
Shuanglong <xjshuanglong@126.com>
2021-01-29 22:50:44 +0800

    element-plus pagination 事件测试

--- src/components/ElementPagination.vue
+++ src/components/ElementPagination.vue
@@ -2,7 +2,7 @@
  * @Author: xautshuanglong
  * @Date: 2021-01-29 00:51:40
  * @LastEditor: xautshuanglong
- * @LastEditTime: 2021-01-29 18:35:47
+ * @LastEditTime: 2021-01-29 22:47:05
  * @FilePath: \shuanglong_admin\src\components\ElementPagination.vue
  * @Description: ElementPlus data pagination
 -->
@@ -15,9 +15,13 @@
     </div>
     <el-pagination layout="prev, pager, next" :total="50" small></el-pagination>
     <el-pagination layout="prev, pager, next" :total="50"></el-pagination>
-    <el-pagination layout="prev, pager, next" :total="500"></el-pagination>
-    <el-pagination layout="prev, pager, next" :total="500" background></el-pagination>
-    <el-pagination layout="total, prev, pager, next, sizes, jumper" :total="500" background></el-pagination>
+    <el-pagination layout="prev, pager, next" :total="500" :page-size="pageSize" :current-page="pageNumber"
+        @size-change="onPageSizeChanged" @current-change="onCurrentPageChanged" />
+    <el-pagination layout="prev, pager, next" :total="500" :page-size="pageSize" :current-page="pageNumber" background
+        @size-change="onPageSizeChanged" @current-change="onCurrentPageChanged"  />
+    <el-pagination layout="total, prev, pager, next, sizes, jumper" background :total="500"
+        :page-size="pageSize" :current-page="pageNumber"
+        @size-change="onPageSizeChanged" @current-change="onCurrentPageChanged" />
     <el-switch v-model="hideSinglePage" active-text="单页隐藏" inactive-text="单页显示"></el-switch>
     <el-pagination layout="prev, pager, next" :total="10" :hide-on-single-page="hideSinglePage"></el-pagination>
   </div>
@@ -31,9 +35,19 @@ export default {
     },
     data () {
         return {
-            pageSize: 0,
+            pageSize: 10,
             pageNumber: 0,
             hideSinglePage: false
+        }
+    },
+    methods: {
+        onPageSizeChanged (pageSize) {
+            // console.log('page size changed :', pageSize)
+            this.pageSize = pageSize
+        },
+        onCurrentPageChanged (pageNumber) {
+            // console.log('current page changed :', pageNumber)
+            this.pageNumber = pageNumber
         }
     }
 }
